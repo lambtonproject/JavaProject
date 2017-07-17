14d873b73ff5a05b5c7164ff01db765e3035475c
lambtonproject <lambton.project@gmail.com>
2017-07-17 01:13:11 -0400

    Update Program.java

--- eCommerce/src/eCommerce/Program.java
+++ eCommerce/src/eCommerce/Program.java
@@ -3,12 +3,7 @@ package eCommerce;
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("ok2");
-=======
-		System.out.println("ok3");
->>>>>>> f316cf7a9e2f125136472db26a03097f60313946
-		
-
+		System.out.println("ok4");
 	}
 
 }
